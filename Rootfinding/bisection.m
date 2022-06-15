18d373cce354b96db2383a4ed340e11fa8047ae1
osccso <osm428@hotmail.com>
2022-06-15 07:57:37 -0500

    bisection done

--- Rootfinding/bisection.m
+++ Rootfinding/bisection.m
@@ -10,14 +10,18 @@
 %fl: function evaluated at xl
 %fu: function evaluated at xu
 %flag: determines if working with absolute(0) o relative error (1)
-function [xr,er,ea] = bisection(xl,xu,f,err,flag)
+function [xr,er,ea,n] = bisection(xl,xu,f,err,flag)
+%default values
+xr = NaN;
+er = NaN;
+ea = NaN;
+%iterations
+n=1;
 %check if values of xl and xu are valid
 if f(xu)*f(xl)>0
-    xr = NaN;
-    er = NaN;
-    ea = NaN;
     error('f(xu)*f(xl)>0 required for method to work');
 end
+%condition to quit while loop
 found = 0;
 xold = xl;
 while ~found
@@ -25,17 +29,30 @@ while ~found
     fr = f(xr);
     fl = f(xl);
     fu = f(xu);
+    %what if root is found
+    if fr == 0
+        return
+    elseif fl == 0
+        xr = xl;
+        return
+    elseif fu == 0
+        xr = xu;
+        return
+    end
     %calculate errors
     er = abs(xr-xold)/xr;
     ea = abs(xr-xold);
+    disp(er)
     %check flag and exit condition
     if flag
         if er < err
             found = 1;
+            return
         end
     else
         if ea < err
             found = 1;
+            return
         end
     end
     %assign new to old
@@ -46,4 +63,6 @@ while ~found
     else
         xu=xr;
     end
+    n=n+1;
+end
 end
